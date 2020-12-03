ef7c1e2df2992ee8cf81fa04219d0593a80da57c
Andy Tockman <andy@tck.mn>
2020-12-03 17:27:48 -0600

    borrow a slice or something instead

--- 03/rs/03.rs
+++ 03/rs/03.rs
@@ -1,14 +1,14 @@
 use std::fs::File;
 use std::io::{self, BufRead};
 
-fn go(input: &Vec<String>, dx: usize, dy: usize) -> usize {
+fn go(input: &[String], dx: usize, dy: usize) -> usize {
     input.iter().step_by(dy).enumerate()
         .filter(|(y, line)| line.chars().cycle().nth(dx*y) == Some('#')).count()
 }
 
 fn main() -> io::Result<()> {
     let f = File::open("input")?;
-    let input = io::BufReader::new(f).lines().flatten().collect();
+    let input: Vec<String> = io::BufReader::new(f).lines().flatten().collect();
 
     println!("{}", go(&input, 3, 1));
     println!("{}", [(1,1),(3,1),(5,1),(7,1),(1,2)]
